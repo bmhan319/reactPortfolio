1d15a2bb6db3d1565309bd947a10f4bc4b5c60e3
Brian Han <bmhan319@hotmail.com>
2019-08-14 14:59:48 -0700

    adjusted component code to make it a functional component rather than a class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,19 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class Navbar extends Component {
-  render() {
-    return(
-      <nav class="navbar">
-        <div class="nav-title-container">
-          <h1 class="nav-title">Brian Han</h1>
-        </div>
-        <ul class="nav-menu-container">
-          <li class="nav-menu-item"><a href="#">About</a></li>
-          <li class="nav-menu-item"><a href="#">Skills</a></li>
-          <li class="nav-menu-item"><a href="#">Work</a></li>
-          <li class="nav-menu-item"><a href="#">Contact</a></li>
-        </ul>
-      </nav>
-    )
-  }
+const Navbar = () => {
+  return(
+    <nav class="navbar">
+      <div class="nav-title-container">
+        <h1 class="nav-title">Brian Han</h1>
+      </div>
+      <ul class="nav-menu-container">
+        <li class="nav-menu-item"><a href="#">About</a></li>
+        <li class="nav-menu-item"><a href="#">Skills</a></li>
+        <li class="nav-menu-item"><a href="#">Work</a></li>
+        <li class="nav-menu-item"><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar;
