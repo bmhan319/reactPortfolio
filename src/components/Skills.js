b11664c944dce263a0f7f370945c1da4e98a2058
Brian Han <bmhan319@hotmail.com>
2019-08-14 15:44:49 -0700

    created a barebones placeholder skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Skills = () => {
+  return(
+    <section class="skills-container">
+      <div className="skills-box">
+        <div className="skills-title-container">
+          <h2 className="skills-title">Web Developer</h2>
+        </div>
+        <div className="skills-list-container">
+          <ul className="skills-list">
+            <li className="skills-item">HTML</li>
+            <li className="skills-item">CSS</li>
+            <li className="skills-item">JS</li>
+            <li className="skills-item">React</li>
+            <li className="skills-item">Git</li>
+            <li className="skills-item">Unix</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
