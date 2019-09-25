7067d01bf9683837849b9bb121c6b6b7650f5b65
Brian Han <bmhan319@hotmail.com>
2019-09-25 16:34:28 -0700

    added component to have page scroll up to the top on home page

--- src/components/ProjectTech.js
+++ src/components/ProjectTech.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const ProjectTech = (props) => {
-  console.log("ai")
+  window.onscroll = scroll
+
+  function scroll() {
+    const elem = document.querySelector("#back-to-top")
+    if (elem == null) {
+      return
+    }
+    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 400)) {
+      elem.classList.add("show")
+      elem.classList.remove("noshow")
+    } else {
+      elem.classList.add("noshow")
+      elem.classList.remove("show")
+    }
+  }
 
   return(
     <div className="project-tech-wrapper">
@@ -39,9 +53,9 @@ const ProjectTech = (props) => {
             </ul>
           </div>
         </div>
-        <div className="back-to-top-container">
-          <a className="back-to-top" href="#top">
-            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 10 129 129" width="30px" height="24px">
+        <div id="back-to-top" className="back-to-top-container noshow">
+          <a className="back-to-top-link" href="#top">
+            <svg fill="#000" xmlns="http://www.w3.org/2000/svg" viewBox="0 10 129 129" width="30px" height="24px">
               <path id="Layer" d="M7.7 88.6C6.9 89.4 6.5 90.4 6.5 91.5C6.5 92.6 6.9 93.6 7.7 94.4C9.3 96 11.9 96 13.5 94.4L64.5 43.4L115.5 94.4C117.1 96 119.7 96 121.3 94.4C122.9 92.8 122.9 90.2 121.3 88.6L67.4 34.6C65.8 33 63.2 33 61.6 34.6L7.7 88.6L7.7 88.6Z" />
             </svg>
           </a>
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 const Summary = () => {
+  window.onscroll = scroll
+
+  function scroll() {
+    const elem = document.querySelector("#back-to-top2")
+    if (elem == null) {
+      return
+    }
+    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 200)) {
+      elem.classList.add("show")
+      elem.classList.remove("noshow")
+    } else {
+      elem.classList.add("noshow")
+      elem.classList.remove("show")
+    }
+  }
+
   return(
     <section className="summary-section">
       <div className="summary-head-section">
@@ -12,6 +28,13 @@ const Summary = () => {
             <p className="work-together-text">I'm currently on the lookout for a fulltime position in Web Development.  What I can bring to the table is a positive attitude, years of experience working within a team environment, and a never ending willingness to constantly learn and improve my abilities.</p>
           </div>
         </div>
+      </div>
+      <div id="back-to-top2" className="back-to-top-container show">
+        <a className="back-to-top-link" href="#top">
+          <svg fill="#000" xmlns="http://www.w3.org/2000/svg" viewBox="0 10 129 129" width="30px" height="24px">
+            <path id="Layer" d="M7.7 88.6C6.9 89.4 6.5 90.4 6.5 91.5C6.5 92.6 6.9 93.6 7.7 94.4C9.3 96 11.9 96 13.5 94.4L64.5 43.4L115.5 94.4C117.1 96 119.7 96 121.3 94.4C122.9 92.8 122.9 90.2 121.3 88.6L67.4 34.6C65.8 33 63.2 33 61.6 34.6L7.7 88.6L7.7 88.6Z" />
+          </svg>
+        </a>
       </div>
     </section>
   )
